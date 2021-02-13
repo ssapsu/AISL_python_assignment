21a3e776e4299c58378cbf3c97b3e2e531720aaa
오현수 <72138421+ssapsu@users.noreply.github.com>
2021-02-13 21:02:06 +0900

    BFS알고리즘을 활용해 환승, 몇 정거장 걸리는지 찾는 기능 추가 예정

--- python.py
+++ python.py
@@ -1,29 +1,29 @@
 class BusData:
-    def __init__(self, lineID, lineName, order, sectionID, butStopID, ARS_ID, butStopName, xCoord, yCoord):
+    def __init__(self, lineID, lineName, order, sectionID, butStopID, ARS_ID, busStopName, xCoord, yCoord):
         self.lineID = lineID
         self.lineName = lineName
         self.order = order
         self.sectionID = sectionID
         self.butStopID = butStopID
         self.ARS_ID = ARS_ID
-        self.butStopName = butStopName
+        self.busStopName = busStopName
         self.xCoord = xCoord
         self.yCoord = yCoord
 
-def makeList(lineID, lineName, order, sectionID, butStopID, ARS_ID, butStopName, xCoord, yCoord, busdataList):
-        busdata=BusData(lineID, lineName, order, sectionID, butStopID, ARS_ID, butStopName, xCoord, yCoord)
+def makeList(lineID, lineName, order, sectionID, butStopID, ARS_ID, busStopName, xCoord, yCoord, busdataList):
+        busdata=BusData(lineID, lineName, order, sectionID, butStopID, ARS_ID, busStopName, xCoord, yCoord)
         busdataList.append(busdata)
 
 busdataList=[]
 
 with open("busdata.csv", 'r', encoding='UTF8') as file:
     for line in file:
-        (lineID, lineName, order, sectionID, butStopID, ARS_ID, butStopName, xCoord, yCoord) = line.strip().split(",")
+        (lineID, lineName, order, sectionID, butStopID, ARS_ID, busStopName, xCoord, yCoord) = line.strip().split(",")
         if (not lineID) or (not lineName) or (not order) or (not sectionID) or (not butStopID) or (not ARS_ID) or (
-        not butStopName) or (not xCoord) or (not yCoord):
+        not busStopName) or (not xCoord) or (not yCoord):
             continue
         else:
-            makeList(lineID, lineName, order, sectionID, butStopID, ARS_ID, butStopName, xCoord, yCoord, busdataList)
+            makeList(lineID, lineName, order, sectionID, butStopID, ARS_ID, busStopName, xCoord, yCoord, busdataList)
 
 def printMenu():
     print('\n'.join([
@@ -38,15 +38,29 @@ def printMenu():
 def option():
     while True:
         printMenu()
-        getNum=input()
+        getNum = input()
         if getNum != "1" and getNum != "2" and getNum != "3":
             print("다시 입력해주세요")
             continue
         elif getNum == "1":
-            print("정류장 이름을 입력하세요 (일부 명칭도 가능): ")
+            getBusStopName = input("정류장 이름을 입력하세요 (일부 명칭도 가능): ")
+            whichBusStopped(getBusStopName, busdataList)
         elif getNum == "2":
-            print("버스노선명을 입력하세요: ")
+            getLineName = input("버스노선명을 입력하세요: ")
+            whereItHadBeen(getLineName, busdataList)
         elif getNum == "3":
             break
 
+def whichBusStopped(getBusStopName, busdataList):
+    for element in busdataList:
+        if getBusStopName in element.busStopName:
+            print(element.lineName)
+
+def whereItHadBeen(getLineName, busdataList):
+    for element in busdataList:
+        if getLineName in element.lineName:
+            print(element.busStopName)
+
+
+'''실제 구현파트'''
 option()
